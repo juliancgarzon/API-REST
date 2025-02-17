29299a4e4508eedc02fa5a9e60c6ec5bdb9ae620
juliancgarzon <juliangarzon@live.com>
2025-02-17 00:49:49 -0500

    create controllers and routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
 
+//routes
+app.use(require('./routes/routes'));
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.listen(3000);
 console.log('server on port 3000');
